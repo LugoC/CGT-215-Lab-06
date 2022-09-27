cc125c348e7b86043d92ca9e344c01ab483946cb
Caleb Lugo <lugoc@purdue.edu>
2022-09-27 14:26:47 -0400

    final commit

--- Lab 06.cpp
+++ Lab 06.cpp
@@ -1,20 +1,50 @@
-// Lab 06.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+#include <SFML/Graphics.hpp>
+using namespace sf;
+using namespace std;
+int main() {
+	string background = "images1/backgrounds/prague.png";
+	string foreground = "images1/characters/yoda.png";
+	Texture backgroundTex;
+	if (!backgroundTex.loadFromFile(background)) {
+		cout << "Couldn't Load Image" << endl;
+		exit(1);
+	}
+	Texture foregroundTex;
+	if (!foregroundTex.loadFromFile(foreground)) {
+		cout << "Couldn't Load Image" << endl;
+		exit(1);
+	}
+	Image backgroundImage;
+	backgroundImage = backgroundTex.copyToImage();
+	Image foregroundImage;
+	foregroundImage = foregroundTex.copyToImage();
+	Vector2u sz = backgroundImage.getSize();
+	for (int y = 0; y < sz.y; y++) {
+		for (int x = 0; x < sz.x; x++) {
 
-int main()
-{
-    std::cout << "Hello World!\n";
+			//creates the reference green color that we will be getting rid of 
+			Color GreenRef(32, 214 , 23, 255);
+
+			//gets the pixel color from the foreground and background
+			Color Yoda = foregroundImage.getPixel(x, y);
+			Color Prague = backgroundImage.getPixel(x, y);
+
+			//if statement that checks if the pixel from foreground is equal to the ref green color
+			if (Yoda == GreenRef) {
+				//if yes, it replaces the pixel color with the background pixle color 
+				foregroundImage.setPixel(x, y, Prague);
+			}
+		}
+	}
+	// By default, just show the foreground image
+	RenderWindow window(VideoMode(1024, 768), "Here's the output");
+	Sprite sprite1;
+	Texture tex1;
+	tex1.loadFromImage(foregroundImage);
+	sprite1.setTexture(tex1);
+	window.clear();
+	window.draw(sprite1);
+	window.display();
+	while (true);
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
